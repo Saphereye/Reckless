52e1c416dc87c616fb70bcebc918068413fdbb23
Adarsh Das <adarshdas950@gmail.com>
2026-04-24 16:41:19 +0530

    depth 12

--- src/search.rs
+++ src/search.rs
@@ -505,7 +505,7 @@ fn search<NODE: NodeType>(
 
     let improving = improvement > 0;
 
-    if !td.stack[ply].follow_pv && !tt_pv && !excluded && depth >= 10 && tt_move.is_null() {
+    if !td.stack[ply].follow_pv && !tt_pv && !excluded && depth >= 12 && tt_move.is_null() {
         depth -= 1;
     }
 
