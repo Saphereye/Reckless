b0acb1c114e1da95cfafaf5b899188d572be945e
Adarsh Das <adarshdas950@gmail.com>
2026-04-25 12:34:46 +0530

    init

--- src/board/see.rs
+++ src/board/see.rs
@@ -21,7 +21,11 @@ impl super::Board {
         }
 
         // In the worst case, we lose a piece, but still end up with a non-negative balance
-        balance -= if mv.is_promotion() { mv.promo_piece_type().value() } else { self.piece_on(mv.from()).value() };
+        balance -= if mv.is_promotion() {
+            mv.promo_piece_type().value() - (15 * PieceType::Pawn.value() / 10)
+        } else {
+            self.piece_on(mv.from()).value()
+        };
 
         if balance >= 0 {
             return true;
