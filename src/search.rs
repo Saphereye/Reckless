216c0bebd911115c0b18a88fd90719a4adb7e2d2
Adarsh Das <adarshdas950@gmail.com>
2026-04-26 18:56:20 +0530

    init

--- src/search.rs
+++ src/search.rs
@@ -658,13 +658,10 @@ fn search<NODE: NodeType>(
         }
 
         if singular_score < singular_beta {
-            let double_margin =
-                204 * NODE::PV as i32 - 16 * tt_move.is_quiet() as i32 - 16 * correction_value.abs() / 128;
             let triple_margin =
                 257 * NODE::PV as i32 - 16 * tt_move.is_quiet() as i32 - 15 * correction_value.abs() / 128 + 32;
 
-            extension = 1;
-            extension += (singular_score < singular_beta - double_margin) as i32;
+            extension = 2;
             extension += (singular_score < singular_beta - triple_margin) as i32;
         }
         // Multi-Cut
