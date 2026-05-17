6ba314d65ab1130fe329ee2a1d08254803390ce1
Adarsh Das <adarshdas950@gmail.com>
2026-05-17 20:33:11 +0530

    Take 1

--- src/search.rs
+++ src/search.rs
@@ -554,8 +554,16 @@ fn search<NODE: NodeType>(
     {
         debug_assert_ne!(td.stack[ply - 1].mv, Move::NULL);
 
+        let prior_history = if td.stack[ply - 1].mv.is_quiet() {
+            td.quiet_history.get(td.board.prior_threats(), !stm, td.stack[ply - 1].mv)
+        } else {
+            0
+        };
+
         let r =
-            (4311 + 1024 * improving as i32 + 260 * depth + 493 * (estimated_score - beta).clamp(0, 1003) / 128) / 1024;
+            (4311 - 200 + 1024 * improving as i32 + 260 * depth + 493 * (estimated_score - beta).clamp(0, 1003) / 128
+                - prior_history / 8)
+                / 1024;
 
         td.stack[ply].conthist = td.stack.sentinel().conthist;
         td.stack[ply].contcorrhist = td.stack.sentinel().contcorrhist;
