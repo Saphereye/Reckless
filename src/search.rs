c8f3d2b252f30fc7b892e27570808b40a3c59187
Adarsh Das <adarshdas950@gmail.com>
2026-06-20 15:53:34 +0530

    Take 1

--- src/search.rs
+++ src/search.rs
@@ -816,7 +816,7 @@ fn search<NODE: NodeType>(
             }
 
             // History Pruning (HP)
-            if !in_check && is_quiet && depth < 5 && history < -948 * depth {
+            if !in_check && is_quiet && depth < 5 && history < -depth * (948 - 200 * improving as i32) {
                 continue;
             }
 
