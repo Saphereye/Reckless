ee464222e73d45782a689eee0eb89419e23b4873
Adarsh Das <adarshdas950@gmail.com>
2026-08-02 11:15:04 +0530

    Take 1
    Bench: 2569707

--- src/search.rs
+++ src/search.rs
@@ -95,7 +95,7 @@ pub fn start(td: &mut ThreadData, report: Report, thread_count: usize) {
             rm.previous_score = rm.score;
         }
 
-        let mut delta = 23 - eval_stability.min(pv_stability).min(7);
+        let mut delta = 23 - eval_stability.min(pv_stability).min(7) + 4 * (td.id > 0) as i32;
         let mut reduction = 0;
 
         for index in 0..td.multi_pv {
