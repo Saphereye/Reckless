03be857032fc729ce4207ff92b72b80cf3ea6297
Adarsh Das <adarshdas950@gmail.com>
2026-08-04 17:24:49 +0530

    Take 1
    Bench: 2556498

--- src/search.rs
+++ src/search.rs
@@ -1309,7 +1309,7 @@ fn qsearch<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, beta: i32, ply: 
 
             // Static Exchange Evaluation Pruning (SEE Pruning)
             if is_valid(eval)
-                && !td.board.see(mv, (alpha - eval) / 8 - correction_value.abs().min(68) - 74 - history / 48)
+                && !td.board.see(mv, (alpha - eval) / 8 - correction_value.abs().min(68) - 74 - history.max(0) / 48)
             {
                 continue;
             }
